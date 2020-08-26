c264311eddec98db5c83b036e0b7e1980b1393ef
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-08-26 23:49:39 +0300

    Yetenekler listelendi.

---
+++ ProjeCv/Controllers/YeteneklerController.cs
@@ -0,0 +1,21 @@
+﻿using ProjeCv.Models.Class;
+using ProjeCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjeCv.Controllers
+{
+    public class YeteneklerController : Controller
+    {
+        DbMvcCvEntities db = new DbMvcCvEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger4 = db.TblSkills.ToList();
+            return View(cs);
+        }
+    }
+}
