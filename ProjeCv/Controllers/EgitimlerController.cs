51ac571343f810f278401273f4874533a30a08f6
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-08-26 23:40:55 +0300

    Eğitimler listelendi.

---
+++ ProjeCv/Controllers/EgitimlerController.cs
@@ -0,0 +1,21 @@
+﻿using ProjeCv.Models.Class;
+using ProjeCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjeCv.Controllers
+{
+    public class EgitimlerController : Controller
+    {
+        DbMvcCvEntities db = new DbMvcCvEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger3 = db.TblEducation.ToList();
+            return View(cs);
+        }
+    }
+}
