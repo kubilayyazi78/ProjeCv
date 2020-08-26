f3d27ce08dd64f3b08641412663caee080058f0c
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-08-26 23:29:09 +0300

    Deneyimler listelendi.

---
+++ ProjeCv/Controllers/DeneyimlerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjeCv.Models.Entity;
+using ProjeCv.Models.Class;
+namespace ProjeCv.Controllers
+{
+    public class DeneyimlerController : Controller
+    {
+        // GET: Deneyimler
+
+        DbMvcCvEntities db = new DbMvcCvEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger2 = db.TblExperience.ToList();
+            return View(cs);
+        }
+    }
+}
